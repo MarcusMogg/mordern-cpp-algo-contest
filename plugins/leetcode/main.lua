452001741377ed17135bb29acf40710a1bb94a84
mogg <m1664907189@gmail.com>
2023-04-02 16:16:30 +0800

    feat: auto build

--- plugins/leetcode/main.lua
+++ plugins/leetcode/main.lua
@@ -1,25 +1,39 @@
+import("core.base.task")
+import("core.project.rule")
 import("core.project.config")
 import("core.project.project")
 import("core.tool.compiler")
 import("lib.detect")
 
-function get_api()
-    local target_name = "leetcode-api"
-    local program_name = project.target(target_name):filename()
-    -- local program_path = path.join(config.buildir(), program_name)
-    local api = detect.find_program(program_name,{
+function GetProgram(target_name, lazy_build)
+    if lazy_build then
+        local program = detect.find_program(target_name,{
+            pathes = { 
+                config.buildir()
+            }})
+        if program ~= nil then
+            return program
+        end
+        print(format("can't find program %s",target_name))
+    end 
+    project.lock()
+    task.run("build", {target = "leetcode-api", all = false})
+    local program = detect.find_program(target_name,{
         pathes = { 
             config.buildir()
         }})
-    if api == nil then
-        raise(format("can't find %s please run xmake build first", program_name))
-    end
+    project.unlock()
 
-    return api
+    
+    if program == nil then
+        raise(format("can't build & find %s", program_name))
+    end
+    
+    return program
 end
 
 
 function main()
-   local api = get_api()
-   os.exec(api)
+    local api = GetProgram("leetcode-api",false)
+    os.exec(api)
 end
