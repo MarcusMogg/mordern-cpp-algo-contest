8b4930e845c14226aeefd698ed199407a3af1979
MarcusMogg <m1664907189@gmail.com>
2023-04-08 22:15:48 +0800

    feat: template

--- lcsrc/two-sum/main.cc
+++ lcsrc/two-sum/main.cc
@@ -1,8 +1,37 @@
+#include <filesystem>
+#include <fstream>
 #include <iostream>
+
+#include "lcsrc/lib/output_helper.h"
+#include "lcsrc/lib/parser.h"
 
 using namespace std;
 
+auto ReadFile(const std::filesystem::path& path) -> std::string {
+  constexpr std::size_t kReadSize = 4096;
+  auto stream = std::ifstream(path.c_str());
+  stream.exceptions(std::ios_base::badbit);
+
+  auto out = std::string();
+  auto buf = std::string(kReadSize, '\0');
+  while (stream.read(buf.data(), kReadSize)) {
+    out.append(buf, 0, stream.gcount());
+  }
+  out.append(buf, 0, stream.gcount());
+  return out;
+}
+
 int main() {
-  cout << "hello world!" << endl;
+  auto test_cases = ReadFile("./simple_test_cases");
+
+  leetcode::lib::
+      TestCase<leetcode::lib::VectorParser<leetcode::lib::IntParser>, leetcode::lib::IntParser>
+          parse;
+
+  auto next_token = leetcode::lib::NextToken(test_cases);
+  auto it = next_token.begin();
+  while (it != next_token.end()) {
+    print(parse.Parse(it));
+  }
   return 0;
 }
--- lcsrc/two-sum/xmake.lua
+++ lcsrc/two-sum/xmake.lua
@@ -1,3 +1,4 @@
 target("two-sum")
     set_kind("binary")
     add_files("*.cc")
+    add_deps("lc-lib")
