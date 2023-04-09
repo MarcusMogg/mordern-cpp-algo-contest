da089eb4f69cf1c39f98a05285d27bba86a028a7
MarcusMogg <m1664907189@gmail.com>
2023-04-09 22:25:23 +0800

    feat: to

--- lcsrc/two-sum/solution.cc
+++ lcsrc/two-sum/solution.cc
@@ -4,9 +4,13 @@
 #include <ranges>
 #include <vector>
 
+#include "lcsrc/lib/stlplus.h"
+
 namespace leetcode::twosum {
+using namespace std::ranges::views;
+using namespace leetcode::lib;
+
 std::vector<int> Solve(const std::vector<int>& nums, const int& target) {
-  using namespace std::ranges::views;
   const auto find_target = [&nums, target](auto&& cur) {
     return std::vector{
         static_cast<int>(cur - nums.begin()),
@@ -14,11 +18,12 @@ std::vector<int> Solve(const std::vector<int>& nums, const int& target) {
     };
   };
   const auto check_second = [&nums](auto&& p) { return p[1] < nums.size(); };
-  for (const auto&& res :
-       iota(nums.begin(), nums.end()) | transform(find_target) | filter(check_second)) {
-    return res;
-  }
-  return {};
+  const auto res = iota(nums.begin(), nums.end())  //
+                   | transform(find_target)        //
+                   | filter(check_second)          //
+                   | take(1)                       //
+                   | to<std::vector<std::vector<int>>>();
+  return res[0];
 }
 
 namespace standard {
