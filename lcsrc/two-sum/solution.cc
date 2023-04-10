658bf3472943071fbd92506749bca3105d35b676
mogg <m1664907189@gmail.com>
2023-04-10 18:12:04 +0800

    feat: more solution

--- lcsrc/two-sum/solution.cc
+++ lcsrc/two-sum/solution.cc
@@ -10,7 +10,10 @@ namespace leetcode::twosum {
 using namespace std::ranges::views;
 using namespace leetcode::lib;
 
-std::vector<int> Solve(const std::vector<int>& nums, const int& target) {
+namespace detail {
+
+// n^2
+std::vector<int> Solve1(const std::vector<int>& nums, const int& target) {
   const auto find_target = [&nums, target](auto&& cur) {
     return std::vector{
         static_cast<int>(cur - nums.begin()),
@@ -18,12 +21,71 @@ std::vector<int> Solve(const std::vector<int>& nums, const int& target) {
     };
   };
   const auto check_second = [&nums](auto&& p) { return p[1] < nums.size(); };
-  const auto res = iota(nums.begin(), nums.end())  //
-                   | transform(find_target)        //
-                   | filter(check_second)          //
-                   | take(1)                       //
-                   | to<std::vector<std::vector<int>>>();
+  const auto res = iota(nums.begin(), nums.end())          //
+                   | transform(find_target)                //
+                   | filter(check_second)                  //
+                   | take(1)                               //
+                   | to<std::vector<std::vector<int>>>();  // c++23
   return res[0];
+}
+
+// n^2
+std::vector<int> Solve2(const std::vector<int>& nums, const int& target) {
+  const auto find_target = [&nums, target](auto&& cur) {
+    return std::vector{
+        static_cast<int>(cur - nums.begin()),
+        static_cast<int>(std::find(cur + 1, nums.end(), target - *cur) - nums.begin()),
+    };
+  };
+  const auto check_second = [&nums](auto&& p) { return p[1] < nums.size(); };
+  return iota(nums.begin(), nums.end())  //
+         | transform(find_target)        //
+         | filter(check_second)          //
+         | take(1)                       //
+         | join                          //
+         | to<std::vector<int>>();       // c++23
+}
+
+// n*logn
+std::vector<int> Solve3(const std::vector<int>& nums, const int& target) {
+  const auto find_target = [&nums, target](auto&& cur) {
+    return std::vector{
+        static_cast<int>(cur - nums.begin()),
+        static_cast<int>(std::upper_bound(cur + 1, nums.end(), target - *cur) - nums.begin()) - 1,
+    };
+  };
+  const auto check_second = [&nums, target](auto&& p) {
+    return p[1] < nums.size() && nums[p[0]] + nums[p[1]] == target;
+  };
+  return iota(nums.begin(), nums.end())  //
+         | transform(find_target)        //
+         | filter(check_second)          //
+         | take(1)                       //
+         | join                          //
+         | to<std::vector<int>>();       // c++23
+}
+// n
+std::vector<int> Solve4(const std::vector<int>& nums, const int& target) {
+  const auto find_target = [&nums, target](auto&& cur) {
+    return std::vector{
+        static_cast<int>(cur - nums.begin()),
+        static_cast<int>(std::upper_bound(cur + 1, nums.end(), target - *cur) - nums.begin()) - 1,
+    };
+  };
+  const auto check_second = [&nums, target](auto&& p) {
+    return p[1] < nums.size() && nums[p[0]] + nums[p[1]] == target;
+  };
+  return iota(nums.begin(), nums.end())  //
+         | transform(find_target)        //
+         | filter(check_second)          //
+         | take(1)                       //
+         | join                          //
+         | to<std::vector<int>>();       // c++23
+}
+}  // namespace detail
+
+std::vector<int> Solve(const std::vector<int>& nums, const int& target) {
+  return detail::Solve3(nums, target);
 }
 
 namespace standard {
