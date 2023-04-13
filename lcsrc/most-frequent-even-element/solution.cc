edd2abe33717a21ddc557c4cf56a09c29da71bb1
mogg <m1664907189@gmail.com>
2023-04-13 09:56:52 +0800

    feat: ranges

--- lcsrc/most-frequent-even-element/solution.cc
+++ lcsrc/most-frequent-even-element/solution.cc
@@ -1,22 +1,20 @@
 #include "solution.h"
 
 #include <map>
+#include <ranges>
 #include <xutility>
 
 namespace leetcode::mostfrequentevenelement {
 int Solve(const std::vector<int>& nums) {
   auto m = std::map<int, int>{};
-  for (const auto& i : nums) {
-    if (i % 2 == 0) {
-      m[i]++;
-    }
+  for (const auto& i : nums | std::ranges::views::filter([](int cur) { return cur % 2 == 0; })) {
+    m[i]++;
   }
   if (m.empty()) {
     m[-1] = 1;
   }
 
-  const auto res = std::ranges::max_element(m, {}, [](const auto& it) { return it.second; });
-  return res->first;
+  return std::ranges::max_element(m, {}, [](const auto& it) { return it.second; })->first;
 }
 
 namespace standard {
