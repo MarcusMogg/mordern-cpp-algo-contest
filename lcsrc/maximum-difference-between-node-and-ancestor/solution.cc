71fe0225b0fadece48fac29542ec4440fe67ca86
mogg <m1664907189@gmail.com>
2023-04-18 14:20:34 +0800

    feat: maximum-difference-between-node-and-ancestor

--- lcsrc/maximum-difference-between-node-and-ancestor/solution.cc
+++ lcsrc/maximum-difference-between-node-and-ancestor/solution.cc
@@ -1,17 +1,44 @@
 #include "solution.h"
 
+#include <algorithm>
+#include <tuple>
+
 namespace leetcode::maximumdifferencebetweennodeandancestor {
 
-int Solve(TreeNode* root) {
-  // TODO(): your solution here
-  return {};
+using Result = std::tuple<int, int, int>;
+
+Result Minmax(TreeNode* root) {
+  if (root == nullptr) {
+    return {};
+  }
+  int minv = root->val;
+  int maxv = root->val;
+  int v = 0;
+
+  const auto merge = [&minv, &maxv, &v, root](const Result& tmp) {
+    minv = std::min(minv, std::get<0>(tmp));
+    maxv = std::max(maxv, std::get<1>(tmp));
+    v = std::max({
+        v,
+        std::abs(minv - root->val),
+        std::abs(maxv - root->val),
+        std::get<2>(tmp),
+    });
+  };
+
+  if (root->left != nullptr) {
+    merge(Minmax(root->left));
+  }
+  if (root->right != nullptr) {
+    merge(Minmax(root->right));
+  }
+  return {minv, maxv, v};
 }
 
+int Solve(TreeNode* root) { return std::get<2>(Minmax(root)); }
+
 namespace standard {
-int Solve(TreeNode* root) {
-  // TODO(): official answer here
-  return {};
-}
+int Solve(TreeNode* root) { return std::get<2>(Minmax(root)); }
 }  // namespace standard
 
 }  // namespace leetcode::maximumdifferencebetweennodeandancestor
