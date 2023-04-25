f96aa9399a5556554f476672d00fbb01f216bfc5
MarcusMogg <m1664907189@gmail.com>
2023-04-25 20:43:22 +0800

    feat: mordern!

--- lcsrc/sort-the-people/solution.cc
+++ lcsrc/sort-the-people/solution.cc
@@ -1,15 +1,26 @@
 #include "solution.h"
 
+#include <vcruntime.h>
+
 #include <algorithm>
 #include <iterator>
+#include <ranges>
 #include <string_view>
 
 namespace leetcode::sortthepeople {
 
+using namespace std::ranges;
+using namespace leetcode::lib;
+
 std::vector<std::string> Solve(
     const std::vector<std::string>& names, const std::vector<int>& heights) {
-  // TODO(): your solution here
-  return {};
+  auto height_index = views::iota(0U, heights.size()) | to<std::vector<size_t>>();
+  std::ranges::sort(
+      height_index, std::ranges::greater{}, [&heights](const size_t i) { return heights[i]; });
+
+  return height_index                                                       //
+         | views::transform([&names](const size_t i) { return names[i]; })  //
+         | to<std::vector<std::string>>();                                  //
 }
 
 namespace standard {
