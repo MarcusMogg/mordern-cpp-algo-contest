5d65536f0494927d7d345e4a8b0b7ff192715687
MarcusMogg <m1664907189@gmail.com>
2023-04-26 21:26:56 +0800

    feat: not mordern

--- lcsrc/maximum-sum-of-two-non-overlapping-subarrays/solution.cc
+++ lcsrc/maximum-sum-of-two-non-overlapping-subarrays/solution.cc
@@ -1,15 +1,36 @@
 #include "solution.h"
 
 #include <algorithm>
+#include <functional>
 #include <iterator>
+#include <ranges>
 #include <type_traits>
 #include <vector>
 
 namespace leetcode::maximumsumoftwononoverlappingsubarrays {
 
+using namespace std::ranges;
+
 int Solve(const std::vector<int>& nums, int firstLen, int secondLen) {
-  // TODO(): your solution here
-  return {};
+  std::vector<int> sum(nums.size() + 1, 0);
+  std::ranges::transform(nums, sum, sum.begin() + 1, std::plus<>());
+
+  const auto calculate_max = [&nums, &sum](int first, int second) {
+    int res = 0;
+    std::vector<int> l_max_value(nums.size(), 0);
+    int r_max_value = 0;
+
+    for (int i : views::iota(first, static_cast<int>(nums.size()))) {
+      l_max_value[i] = std::max(l_max_value[i - 1], sum[i] - sum[i - first]);
+    }
+    for (int i = static_cast<int>(nums.size()) - second; i > 0; --i) {
+      r_max_value = std::max(r_max_value, sum[i + second] - sum[i]);
+      res = std::max(res, r_max_value + l_max_value[i]);
+    }
+    return res;
+  };
+
+  return std::max(calculate_max(firstLen, secondLen), calculate_max(secondLen, firstLen));
 }
 
 namespace standard {
