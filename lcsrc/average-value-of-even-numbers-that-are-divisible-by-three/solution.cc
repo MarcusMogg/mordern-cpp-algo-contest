91d1615f0f920ae8f70eb9a3ca4bee23d61cae8d
mogg <m1664907189@gmail.com>
2023-05-29 09:24:45 +0800

    mordern!

--- lcsrc/average-value-of-even-numbers-that-are-divisible-by-three/solution.cc
+++ lcsrc/average-value-of-even-numbers-that-are-divisible-by-three/solution.cc
@@ -1,13 +1,26 @@
 #include "solution.h"
 
 #include <numeric>
+#include <ranges>
 #include <utility>
 
 namespace leetcode::averagevalueofevennumbersthataredivisiblebythree {
 
 int Solve(const std::vector<int>& nums) {
-  // TODO(): your solution here
-  return {};
+  const auto res = std::ranges::fold_left(
+      nums                                                                        //
+          | std::ranges::views::filter([](const auto& i) { return i % 6 == 0; })  //
+          | std::ranges::views::transform([](const auto& i) {
+              return std::pair{i, 1};
+            }),
+      std::pair{0, 0},
+      [](const auto& l, const auto& r) {
+        return std::pair{l.first + r.first, l.second + r.second};
+      });
+  if (res.second == 0) {
+    return 0;
+  }
+  return res.first / res.second;
 }
 
 namespace standard {
